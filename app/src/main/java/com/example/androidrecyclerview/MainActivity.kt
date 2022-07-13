27cc6d8e72a98816287f826a43996c474a58f7c3
Jjol_twi <hgymin3129@gmail.com>
2022-07-13 15:15:17 +0900

    now we ready

---
+++ app/src/main/java/com/example/androidrecyclerview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.androidrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        recycler_view.visibility = View.GONE
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
